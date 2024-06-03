00ba65dd02912299c323ddea6f93920215a5f67a
HarisImamovicIBU <haris.imamovic@stu.ibu.edu.ba>
2024-06-03 22:05:39 +0200

    Project 3 commit. Working on section projects.

--- Project3/my-app/src/pages/Projects/Projects.js
+++ Project3/my-app/src/pages/Projects/Projects.js
@@ -1,9 +1,24 @@
-import styles from './ProjectsStyles.module.css'
-function Projects(){
-    return(
+import styles from './ProjectsStyles.module.css';
+import sportImage from '../../assets/sport-image.svg';
+function Projects() {
+    return (
         <section id="projects" className={styles.container}>
             <h2 className="title">Projects</h2>
+            <br></br>
+            <div className={styles.projects_container}>
+                <div className={styles.project_item}>
+                    <img src={sportImage} alt="Project 1" className={styles.project_image} />
+                </div>
+                <div className={styles.project_item}>
+                    <img src={sportImage} alt="Project 2" className={styles.project_image} />
+                </div>
+                <div className={styles.project_item}>
+                    <img src={sportImage} alt="Project 3" className={styles.project_image} />
+                </div>
+
+            </div>
         </section>
     );
 }
+
 export default Projects;
